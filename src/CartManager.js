19c8520e27fbbdacbbdac88f1b49f8b732fb477b
carolina <carolinaperotti16@gmail.com>
2023-04-09 03:52:50 -0300

    endpoints carts

--- src/CartManager.js
+++ src/CartManager.js
@@ -21,7 +21,7 @@ export default class CartsManager {
             const cartsParse = await this.getCarts();
 
             const idCart = cartsParse.length === 0 ? 1 : cartsParse[cartsParse.length-1].id +1
-            cartsParse.push({...product, id: idCart,})
+            cartsParse.push({id: idCart, products:[]})
 
             let cartsJson = JSON.stringify(cartsParse, null, 2)
             await fs.promises.writeFile(this.path, cartsJson, 'utf-8')
@@ -50,7 +50,7 @@ export default class CartsManager {
 
             const cartSearch = cartsParse.find(cart => cart.id === id)
             if(cartSearch){
-                return cartSearch
+                return cartSearch.products
             }else{
                 return "Not found"
             }
@@ -60,9 +60,56 @@ export default class CartsManager {
         }
     }
 
-    addProductToCart = async(cartId, productId, dataAdd) =>{
+    addProductToCart = async(cartId, dataProduct) =>{
         try{
             const cartsParse = await this.getCarts()
+
+            //reveer como hacerlo
+            cartsParse.map((cart)=>{
+                if(cart.id === cartId){
+                    if(cart.products.length === 0){
+                        return{
+                            ...cart,
+                            products: [{product: dataProduct.id, quantity: dataProduct.stock}]
+                        }
+                    }else{
+                        const productSearch = cart.products.find(product => product === dataProduct.id);
+
+                        if(productSearch){
+                            return{
+                                ...cart,
+                                products: cart.products.map((prod)=>{
+                                    if(prod.product === dataProduct.id){
+                                        return{
+                                            ...prod,
+                                            quantity: prod.quantity + dataProduct.stock
+                                        }
+                                    }
+                                    return prod
+                                })
+                            }
+                        }else{
+                            return{
+                                ...cart,
+                                products: [...cart.products, {product: productId, quantity:dataProduct.stock}]
+                            }
+                        }
+
+                        
+                    }
+                }
+                return cart
+            })
+
+            // const cartSearch = cartsParse.find(cart => cart.id === cartId)
+
+            // if(cartSearch){
+            //     const productSearch = cartSearch.products.find(product=> product.product === productId)
+
+            //     if(!productSearch){
+
+            //     }
+            // }
 
             // let productsJson = JSON.stringify(updateProduct, null, 2)
             // await fs.promises.writeFile(this.path, productsJson, 'utf-8')
--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -1,17 +1,29 @@
 import { Router } from "express";
+import CartsManager from '../CartManager.js'
 
 const router = Router()
 
-router.post('/', (req, res)=>{
+const carts = new CartsManager('src/dataCarts.json')
 
+router.post('/', (req, res)=>{
+    carts.addCart().then(response=>{
+        res.send(response);
+    })
+    .catch((error)=> console.log(error));
+})
+
+router.get('/:cid', (req, res)=>{
+    let {cid} = req.params;
+    carts.getCartById(parseInt(cid)).then((response)=>{
+        res.send(response)
+    })
+    .catch((error)=> console.log(error))
+    
 })
 
 router.post('/:cid/product/:pid', (req, res)=>{
     
 })
 
-router.get('/:cid', (req, res)=>{
-    
-})
 
 export default router;
