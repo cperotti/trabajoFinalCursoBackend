d4fdfb29b6217ba833b182c082e28fa88d0364d9
carolina <carolinaperotti16@gmail.com>
2023-04-09 23:46:49 -0300

    public en app

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@ import express from 'express';
 import productsRouter from './routes/products.router.js';
 import cartsRouter from './routes/carts.router.js';
 
+const {pathname: root} = new URL('../src', import.meta.url)
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use(express.static(root +'public'))
 
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
