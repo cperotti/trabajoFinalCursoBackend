509bcdee181f8409564fef469cc79ed883bfe98b
carolina <carolinaperotti16@gmail.com>
2023-08-20 00:48:34 -0300

    test cart

--- test/Carts.test.js
+++ test/Carts.test.js
@@ -11,15 +11,32 @@ describe('Testing de Cart Dao', ()=> {
         this.cartDao = new CartManagerMongo();
     })
     beforeEach(function(){
-        // mongoose.connection.collections.users.drop()
+        // mongoose.connection.collections.carts.drop()
         this.timeout(2000)
     })
-    it('El dao debe poder obtener todos los carts en un arreglo', async function(){
-        const result = await this.cartDao.getCarts({})
-        console.log(result)
-        expect(result).to.be.deep.equal([])
-        expect(result).deep.equal([])
-        expect(Array.isArray(result)).to.be.ok
-        expect(Array.isArray(result)).to.be.equals(true)
+    // it('El dao debe poder obtener todos los carts en un arreglo', async function(){
+    //     const result = await this.cartDao.getCarts({})
+    //     expect(result).to.be.deep.equal([])
+    // }).timeout(20000)
+    // it('El dao debe poder agregar un producto al carrito', async function(){
+    //     const newCart= {products: []}
+    //     const cart = await this.cartDao.addCart(newCart)
+    //     await this.cartDao.addProductToCart(cart._id, '64e17d09550db00f0b6b7761')
+    //     const cartById = await this.cartDao.getCartById(cart._id)
+    //     expect(cartById.products[0].product._id.toString()).to.equal('64e17d09550db00f0b6b7761');
+    // }).timeout(20000)
+    // it('El dao debe poder editar un producto que ya este agregado en el carrito', async function(){
+    //     const updateDataProduct = {
+    //         quantity:2
+    //     }
+    //     await this.cartDao.updateCartProduct('64e18294494ae54cc8c2c2c6', '64e17d09550db00f0b6b7761',updateDataProduct)
+    //     const cartById = await this.cartDao.getCartById('64e18294494ae54cc8c2c2c6')
+    //     expect(cartById.products[0]).to.have.property('quantity', updateDataProduct.quantity)
+    // }).timeout(20000)
+    it('El dao debe poder eliminar un producto que ya este agregado en el carrito', async function(){
+        await this.cartDao.deleteProductToCart('64e18294494ae54cc8c2c2c6','64e17d09550db00f0b6b7761' )
+        const cartById = await this.cartDao.getCartById('64e18294494ae54cc8c2c2c6')
+        const has_product = cartById.products.find(el=> el._id.toString() === '64e17d09550db00f0b6b7761' )
+        expect(has_product).to.equal(undefined);
     }).timeout(20000)
 })
--- test/Supertest.test.js
+++ test/Supertest.test.js
@@ -9,25 +9,27 @@ describe('Testing de ecommerce', ()=>{
         it('El endpoint de Post /api/products debe crear un producto correctamente', async ()=>{
             const userAdminMock = {
                 email: "coderadmin@admin.com",
-                password: "coderadmin123"
+                password: "coderadmin123",
             }
             const result = await requester.post('/api/session/login').send(userAdminMock)
+            // const cookieResult = result.headers['set-cookie'][0]
+            // expect(cookieResult).to.be.ok
             console.log(result)
-            const productMock = {
-                title: 'Producto prueba',
-                description: 'Es un producto de prueba',
-                thumbnail: '', 
-                price: 100,
-                stock: 37,
-                code: '015',
-                category: 'prueba',
-                status:true
-            }
-            const {statusCode, _body, ok} = await requester.post('/api/products/').send(productMock)
-            console.log(statusCode)
-            console.log(_body)
-            console.log(ok)
-            expect(_body.payload).to.have.property('_id')
+            // const productMock = {
+            //     title: 'Producto prueba',
+            //     description: 'Es un producto de prueba',
+            //     thumbnail: '', 
+            //     price: 100,
+            //     stock: 37,
+            //     code: '015',
+            //     category: 'prueba',
+            //     status:true
+            // }
+            // const {statusCode, _body, ok} = await requester.post('/api/products/').send(productMock)
+            // console.log(statusCode)
+            // console.log(_body)
+            // console.log(ok)
+            // expect(_body.payload).to.have.property('_id')
         })
         
     })
