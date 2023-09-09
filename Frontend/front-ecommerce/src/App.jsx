186338db4b5cf4a4736172b963a22fb2ce9e7239
cperotti <carolinaperotti16@gmail.com>
2023-09-08 23:44:45 -0300

    prueba front

---
+++ Frontend/front-ecommerce/src/App.jsx
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import Login from "./pages/Login";
+
+function App() {
+
+  return (
+    <>
+      <Login />
+    </>
+  )
+}
+
+export default App
--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,5 +1,5 @@
 import nodemailer from 'nodemailer';
-import { configServer } from '../configServer/configServer'
+import { configServer } from '../configServer/configServer.js'
 
 const transport = nodemailer.createTransport({
     service: 'gmail',
